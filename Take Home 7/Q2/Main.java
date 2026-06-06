9188e92a9b86ea636964368e65fbd97505067f53
Hasitha Jayakody <hheshanj@gmail.com>
2026-06-06 21:38:48 +0530

    Add Printer and Main classes with printData methods for various data types

---
+++ Take Home 7/Q2/Main.java
@@ -0,0 +1,13 @@
+package Q2;
+
+public class Main {
+  public static void main(String[] args) {
+    Printer print = new Printer();
+
+    print.printData("Heshan", 10);
+    System.out.println(print.printData(69));
+    System.out.println(print.printData("gay"));
+    System.out.println(print.printData(true));
+
+  }  
+}
--- Take Home 7/Q2/Printer.java
+++ Take Home 7/Q2/Printer.java
@@ -1,7 +1,19 @@
 package Q2;
 
 public class Printer {
-    String printData(){
-        
+    String printData(String text){
+        System.out.println("Printing Text");
+        return text;
+    }
+    int printData(int number){
+        System.out.println("Printing Number: ");
+        return number;
+    }
+    void printData(String text, int copies){
+        System.out.println("Printing " + copies + " copies of " + text);
+    }
+    boolean printData(boolean isDuplex){
+        System.out.println("Duplex printing is: ");
+        return isDuplex;
     }
 }
