bce0712e2c7346b81fec917e263091b501edc0a2
heshan-jj <jayakodyheshan@gmail.com>
2026-06-23 15:58:55 +0530

    day09

---
+++ Labs/Day09/Q02/Staff.java
@@ -0,0 +1,27 @@
+package Q02;
+
+public class Staff{
+    private String name;
+    private int basicSalary;
+
+
+    public Staff(String name, int basicSalary){
+        this.name = name;
+        this.basicSalary = basicSalary;
+
+    }
+
+    public String getname(){
+        return name;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+
+    public int getsalary(){
+        return basicSalary;
+    }
+    public void getsalary(int basicSalary){
+        this
+    }
+}
